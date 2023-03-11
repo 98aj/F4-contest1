d4c70c15b14ed809c862c054144098e910661bf2
98aj <103376356+98aj@users.noreply.github.com>
2023-03-11 22:59:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './component/Form';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Form/>
+      
+    </>
+  );
+}
+
+export default App;
